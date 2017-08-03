c57e42c101f42fb071456eb2b2ca6a03a309d8fc
rajeev0739 <rajeev0739@gmal.com>
2017-08-03 07:45:14 +0530

    first commit

---
+++ src/main/java/com/rajeev/servlet/ServerTime.java
@@ -0,0 +1,44 @@
+package com.rajeev.servlet;
+
+import java.io.IOException;
+import java.util.Calendar;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ServerTime
+ */
+@WebServlet("/servertime")
+public class ServerTime extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ServerTime() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	//	response.getWriter().append("Server Time: "+new Date()).append(request.getContextPath());
+		response.getWriter().append("Server Time: "+Calendar.getInstance().getTime());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
